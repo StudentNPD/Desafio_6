b64cc47af13ce0a367d8830ead31a3d7347ba6d2
Francisco Colomer B. <francisco.colomer@gmail.com>
2024-08-13 21:41:49 -0400

    Update masa.py
    se le agrega docstring tipo google

--- masa.py
+++ masa.py
@@ -1,4 +1,17 @@
-def tipo_masa(orden, eleccion):
+def tipo_masa(orden: dict, eleccion: str) -> dict:
+    """
+    Agrega el tipo de masa seleccionado a la orden.
+
+    Args:
+        orden (dict): Un diccionario que representa la orden actual.
+        eleccion (str): Una letra que representa la elección de masa ('T', 'D', o 'B').
+
+    Returns:
+        dict: El diccionario de la orden actualizado con el tipo de masa.
+
+    Raises:
+        None, pero imprime un mensaje si la elección no es válida.
+    """
     masas = {
         'T': 'Masa Tradicional',
         'D': 'Masa Delgada',
