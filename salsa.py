6595ea62d5422828d2d4d59d36bd2d6ccfdccc65
Francisco Colomer B. <francisco.colomer@gmail.com>
2024-08-13 21:43:18 -0400

    Update salsa.py
    se agrega docstrings tipo google

--- salsa.py
+++ salsa.py
@@ -1,4 +1,17 @@
-def tipo_salsa(orden, eleccion):
+def tipo_salsa(orden: dict, eleccion: str) -> dict:
+    """
+    Agrega el tipo de salsa seleccionado a la orden.
+
+    Args:
+        orden (dict): Un diccionario que representa la orden actual.
+        eleccion (str): Una letra que representa la elección de salsa ('T', 'A', 'B', o 'P').
+
+    Returns:
+        dict: El diccionario de la orden actualizado con el tipo de salsa.
+
+    Raises:
+        None, pero imprime un mensaje si la elección no es válida.
+    """
     salsas = {
         'T': 'Salsa de Tomate',
         'A': 'Salsa Alfredo',
