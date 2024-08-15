e295746469c129c64aab1d82dcf89adb6e0da811
Natalia <90008571+StudentNPD@users.noreply.github.com>
2024-08-15 03:06:34 -0400

    Delete suma_ingredientes.py

--- suma_ingredientes.py
+++
@@ -1,7 +0,0 @@
-def agregar_ingredientes(ingredientes,lista_ingredientes, eleccion):
-    
-    
-    ingredientes.append(lista_ingredientes[eleccion-1])
-    print(f'Se ha agregado {lista_ingredientes[eleccion-1]}')
-
-    return ingredientes
