19bb28ed10cf5e0177b47b0eaa0cbdd8340e7dee
Cy5k0 <francisco.colomer@gmail.com>
2024-08-16 21:05:18 -0400

    update funcion agregar_ingredientes

--- ingredientes.py
+++ ingredientes.py
@@ -1,22 +1,25 @@
 # Que se permita modificar ingredientes (agregar y eliminar). Actualmente, la
 # pizzería trabaja con los siguientes ingredientes: Tomate, Champiñones,
 # Aceituna, Cebolla, Pollo, Jamón, Carne, Tocino, Queso
-agregados={
-    1:'Tomate',
-    2:'Champiñones',
-    3:'Aceituna',
-    4:'Cebolla',
-    5:'Pollo',
-    6:'Jamón',
-    7:'Carne',
-    8:'Tocino',
-    9:'Queso',
+agregados = {
+    1: "Tomate",
+    2: "Champiñones",
+    3: "Aceituna",
+    4: "Cebolla",
+    5: "Pollo",
+    6: "Jamón",
+    7: "Carne",
+    8: "Tocino",
+    9: "Queso",
 }
 
+
 def agregar_ingrediente(ingredientes):
-    i=0
-    while i < 3 :
-        eleccion = int(input('''Seleccione su Ingrediente:
+    i = 0
+    while i < 3:
+        eleccion = int(
+            input(
+                """Seleccione su Ingrediente:
         1) Tomate
         2) Champiñones
         3) Aceituna
@@ -26,33 +29,66 @@ def agregar_ingrediente(ingredientes):
         7) Carne
         8) Tocino
         9) Queso\n
-        > '''))
+        > """
+            )
+        )
         ingredientes.append(agregados[eleccion])
         print(f"Haz Agregado: {ingredientes}")
-        i = i +1
-        
+        i = i + 1
+
         if i == 3:
             break
-        
-        pregunta = str(input('¿Desea agregar otro ingrediente? Escriba SI o NO: '))
-        
-        if pregunta == 'SI':
-            continue
-        elif pregunta == 'NO':
-            break
-        else:
-            print('Por favor ingrese algo')
-        
+
+        while True:
+            pregunta = input("¿Desea agregar otro ingrediente? Escriba s / n: ").lower()
+            if pregunta == "s":
+                break
+            elif pregunta == "n":
+                return ingredientes
+            else:
+                print("Por favor ingrese s / n")
+
     return ingredientes
+
+
+# def agregar_ingrediente(ingredientes):
+#     i=0
+#     while i < 3 :
+#         eleccion = int(input('''Seleccione su Ingrediente:
+#         1) Tomate
+#         2) Champiñones
+#         3) Aceituna
+#         4) Cebolla
+#         5) Pollo
+#         6) Jamón
+#         7) Carne
+#         8) Tocino
+#         9) Queso\n
+#         > '''))
+#         ingredientes.append(agregados[eleccion])
+#         print(f"Haz Agregado: {ingredientes}")
+#         i = i + 1
+
+#         if i == 3:
+#             break
+
+#         pregunta = input('¿Desea agregar otro ingrediente? Escriba SI o NO: ').lower
+
+#         if pregunta == 'S':
+#             continue
+#         elif pregunta == 'N':
+#             break
+#         else:
+#             print('Por favor ingrese algo')
+
+#     return ingredientes
+
 
 def eliminar_ingrediente(ingredientes):
     i = 0
     for ingrediente in ingredientes:
         print(i, ingrediente)
-        i+=1
-    eliminar= int(input('Escoga Ingrediente que desea eliminar:'))
+        i += 1
+    eliminar = int(input("Escoga Ingrediente que desea eliminar:"))
     ingredientes.remove(ingredientes[eliminar])
     return ingredientes
-        
-        
-
--- pizzeria.py
+++ pizzeria.py
@@ -1,25 +1,31 @@
-#Desafio 6 Modulo 3
+# Desafio 6 Modulo 3
 import funciones
 import masa
 import salsa
-#import suma_ingredientes
+
+# import suma_ingredientes
 import ingredientes as funcion_agregar_eliminar
 
-ingredientes= ['Queso']
-orden = {'masa': 'Masa Tradicional','salsa': 'Salsa de Tomate','ingredientes': ingredientes}
-#lista_ingredientes = ['Tomate','Champiñones','Aceituna','Cebolla','Pollo','Jamon','Carne','Tocino','Queso']
+ingredientes = ["Queso"]
+orden = {
+    "masa": "Masa Tradicional",
+    "salsa": "Salsa de Tomate",
+    "ingredientes": ingredientes,
+}
+# lista_ingredientes = ['Tomate','Champiñones','Aceituna','Cebolla','Pollo','Jamon','Carne','Tocino','Queso']
 
-#agregar=[]
+# agregar=[]
 
 print("************************")
 print("Bienvenidos Pizzeria JAT")
 print("************************")
 
 print("Orden Básica:")
-funciones.mostrar(orden,ingredientes)
+funciones.mostrar(orden, ingredientes)
 
 while True:
-    opcion = input('''Seleccione
+    opcion = input(
+        """Seleccione
     1. Cambiar tipo de Masa
     2. Cambiar tipo de Salsa
     3. Agregar Ingredientes
@@ -27,80 +33,83 @@ while True:
     5. Consultar ingredientes de la pizza
     6. Ordenar con los Ingredientes Actuales
     0. Cancelar pedido.
-    > ''')
+    > """
+    )
 
-    if opcion == '1':
-        eleccion = input("""Escoja el tipo de Masa:
+    if opcion == "1":
+        eleccion = input(
+            """Escoja el tipo de Masa:
         T). Tradicional
         D). Delgada
         B). Bordes de Queso
-        > """).upper()
+        > """
+        ).upper()
         orden = masa.tipo_masa(orden, eleccion)
-        aux=input("--->Presione ENTER para continuar")
+        aux = input("--->Presione ENTER para continuar")
 
-    elif opcion == '2':
-        eleccion = input('''Seleccione su tipo de Salsa:
+    elif opcion == "2":
+        eleccion = input(
+            """Seleccione su tipo de Salsa:
         T). Tomate
         A). Alfredo
         B). Barbecue
         P). Pesto
-        > ''').upper()
+        > """
+        ).upper()
         orden = salsa.tipo_salsa(orden, eleccion)
 
-    elif opcion == '3':
-        
-        #ingredientes.agregar_ingrediente()
-        
-        #i=1
-        #for elemento in  lista_ingredientes:
-        #    print(f"{i}). {lista_ingredientes[i-1]}") 
-        #    i=i+
-        
-#        1). Tomate
-#        2). Champiñones
-#        3). Aceituna
-#        4). Cebolla
-#        5). Pollo
-#        6). Jamón
-#        7). Carne
-#        8). Tocino
-#        9). Queso
-#        > '''))
-        #ingredientes = agregar_ingredientes(ingredientes,lista_ingredientes,eleccion)
-        ingredientes = funcion_agregar_eliminar.agregar_ingrediente(ingredientes)
-        
+    elif opcion == "3":
 
-    elif opcion == '4':
-        #eleccion = int(input('''Seleccione qué ingrediente quitar:
-        #1). Tomate
-        #2). Champiñones
-        #3). Aceituna
-        #4). Cebolla
-        #5). Pollo
-        #6). Jamón
-        #7). Carne
-        #8). Tocino
-        #9). Queso
-        #> '''))
-        #orden = quitar_ingrediente(orden,eleccion)
+        # ingredientes.agregar_ingrediente()
+
+        # i=1
+        # for elemento in  lista_ingredientes:
+        #    print(f"{i}). {lista_ingredientes[i-1]}")
+        #    i=i+
+
+        #        1). Tomate
+        #        2). Champiñones
+        #        3). Aceituna
+        #        4). Cebolla
+        #        5). Pollo
+        #        6). Jamón
+        #        7). Carne
+        #        8). Tocino
+        #        9). Queso
+        #        > '''))
+        # ingredientes = agregar_ingredientes(ingredientes,lista_ingredientes,eleccion)
+        ingredientes = funcion_agregar_eliminar.agregar_ingrediente(ingredientes)
+
+    elif opcion == "4":
+        # eleccion = int(input('''Seleccione qué ingrediente quitar:
+        # 1). Tomate
+        # 2). Champiñones
+        # 3). Aceituna
+        # 4). Cebolla
+        # 5). Pollo
+        # 6). Jamón
+        # 7). Carne
+        # 8). Tocino
+        # 9). Queso
+        # > '''))
+        # orden = quitar_ingrediente(orden,eleccion)
         ingredientes = funcion_agregar_eliminar.eliminar_ingrediente(ingredientes)
 
-
-    elif opcion == '5':
+    elif opcion == "5":
         funciones.mostrar(orden, ingredientes)
-        
-        aux=input("--->Presione ENTER para continuar")
 
-    elif opcion == '6':
+        aux = input("--->Presione ENTER para continuar")
+
+    elif opcion == "6":
         tiempo = funciones.estimar_tiempo(ingredientes)
-        print(f'Su Pizza estará lista en {tiempo} minutos')
-        ordenar = input('Desea ordenar ahora S/N: ').upper()
-        if ordenar == 'S':
-            print('Gracias por comprar en Pizza JAT')
-            print('Disfrute su Pizza!!!!!!')
+        print(f"Su Pizza estará lista en {tiempo} minutos")
+        ordenar = input("Desea ordenar ahora S/N: ").upper()
+        if ordenar == "S":
+            print("Gracias por comprar en Pizza JAT")
+            print("Disfrute su Pizza!!!!!!")
             exit()
-        aux=input("--->Presione Enter para continuar")
+        aux = input("--->Presione Enter para continuar")
 
-    elif opcion == '0':
-        print('Su pedido ha sido cancelado. Gracias por Preferirnos Pizza JAT')
-        exit()    
+    elif opcion == "0":
+        print("Su pedido ha sido cancelado. Gracias por Preferirnos Pizza JAT")
+        exit()
